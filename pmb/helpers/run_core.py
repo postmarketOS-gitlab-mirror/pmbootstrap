1a00c04f74ae0aa871e38b8b5add421565ddc0ee
Clayton Craft <clayton@craftyguy.net>
2023-07-12 19:18:21 -0700

    pmb.helpers.run_core: always configure proxy vars if set in environment
    By configuring proxy vars in run_core.core, we can ensure that the
    environment is always aware of any configured proxy, whether it's
    running natively or in a chroot.
    
    fixes https://gitlab.com/postmarketOS/pmbootstrap/-/issues/2249
    
    Reviewed-by: Oliver Smith <ollieparanoid@postmarketos.org>
    Link: https://lists.sr.ht/~postmarketos/pmbootstrap-devel/%3C20230713021822.15223-2-clayton@craftyguy.net%3E

--- pmb/helpers/run_core.py
+++ pmb/helpers/run_core.py
@@ -310,6 +310,13 @@ def core(args, log_message, cmd, working_dir=None, output="log",
     """
     sanity_checks(output, output_return, check)
 
+    # Preserve proxy environment variables
+    for var in ["FTP_PROXY", "ftp_proxy", "HTTP_PROXY", "http_proxy",
+                "HTTPS_PROXY", "https_proxy", "HTTP_PROXY_AUTH"]:
+        if var in os.environ:
+            # Prepend setting var to cmd string
+            cmd = f"{var}={os.environ[var]} {cmd}"
+
     if args.sudo_timer and sudo:
         sudo_timer_start()
 
