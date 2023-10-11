6560b784d4fe47b904c755bd9d11957f53c645f2
Andras Sebok <sebokandris2009@gmail.com>
2023-10-11 14:10:20 +0200

    parse/init: allow importing arguments_install and arguments_flasher
    These changes are required for gpmbootstrap to work.
    
    Reviewed-by: Oliver Smith <ollieparanoid@postmarketos.org>
    Link: https://lists.sr.ht/~postmarketos/pmbootstrap-devel/%3C169765669479.3595.15602576022263389645-1@git.sr.ht%3E

--- pmb/parse/__init__.py
+++ pmb/parse/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2023 Oliver Smith
 # SPDX-License-Identifier: GPL-3.0-or-later
-from pmb.parse.arguments import arguments
+from pmb.parse.arguments import arguments, arguments_install, arguments_flasher
 from pmb.parse._apkbuild import apkbuild
 from pmb.parse._apkbuild import function_body
 from pmb.parse.binfmt_info import binfmt_info
