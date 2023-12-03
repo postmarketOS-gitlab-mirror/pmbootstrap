postmarketOS-gitlab-mirror/pmbootstrap

commit 41daa850d731f563bf320702b2d719b743416139

parse.arguments: Allow providing multiple packages for kconfig_check
It can be quite useful to check the configs for a list of packages, so
adjust the argument parsing code to support that.

Reviewed-by: Oliver Smith <ollieparanoid@postmarketos.org>
Tested-by: Oliver Smith <ollieparanoid@postmarketos.org>
Reviewed-by: Clayton Craft <clayton@craftyguy.net>
Link: https://lists.sr.ht/~postmarketos/pmbootstrap-devel/%3C20231028085901.35205-1-luca@z3ntu.xyz%3E

pmb/helpers/frontend.py
@@ -393,10 +393,8 @@ def kconfig(args):
             raise RuntimeError("kconfig check failed!")
 
         # Default to all kernel packages
-        packages = []
-        if args.package:
-            packages = [args.package]
-        else:
+        packages = args.package
+        if not args.package:
             for aport in pmb.helpers.pmaports.get_list(args):
                 if aport.startswith("linux-"):
                     packages.append(aport.split("linux-")[1])

pmb/parse/arguments.py
@@ -476,7 +476,7 @@ def arguments_kconfig(subparser):
         check.add_argument(f"--{name}", action="store_true",
                            dest=f"kconfig_check_{name}",
                            help=f"check options needed for {name} too")
-    add_kernel_arg(check)
+    add_kernel_arg(check, nargs="*")
 
     # "pmbootstrap kconfig edit"
     edit = sub.add_parser("edit", help="edit kernel aport config")

commit 1f1bb8e7e0d8f8914617e8084c116f4daf9672b1

pmb.helpers.frontend: Convert kconfig check --file argument
Stop using --file as a boolean argument and just use the value behind
the argument as file to check. This also unbreaks this functionality
after the recent change now that args.package is a list since you can
pass multiple kernel packages as arguments.

Fixes: 41daa850 ("parse.arguments: Allow providing multiple packages for kconfig_check")
Closes: https://gitlab.com/postmarketOS/pmbootstrap/-/issues/2282
Reviewed-by: Oliver Smith <ollieparanoid@postmarketos.org>
Link: https://lists.sr.ht/~postmarketos/pmbootstrap-devel/%3C20231203094559.413672-1-luca@z3ntu.xyz%3E

pmb/helpers/frontend.py
@@ -386,7 +386,7 @@ def kconfig(args):
 
         # Handle passing a file directly
         if args.file:
-            if pmb.parse.kconfig.check_file(args.package, components_list,
+            if pmb.parse.kconfig.check_file(args.file, components_list,
                                             details=details):
                 logging.info("kconfig check succeeded!")
                 return

pmb/parse/arguments.py
@@ -468,8 +468,8 @@ def arguments_kconfig(subparser):
                        " kernels, even the ones that would be ignored by"
                        " default")
     check.add_argument("--arch", choices=arch_choices, dest="arch")
-    check.add_argument("--file", action="store_true", help="check a file"
-                       " directly instead of a config in a package")
+    check.add_argument("--file", help="check a file directly instead of a"
+                       " config in a package")
     check.add_argument("--no-details", action="store_false",
                        dest="kconfig_check_details",
                        help="print one generic error per component instead of"